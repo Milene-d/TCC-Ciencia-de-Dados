7c5f1b92f2a58935df84769820acc30f100be733
Milene Dias <77759873+Milene-d@users.noreply.github.com>
2021-09-26 15:58:41 -0300

    Create Pacote necessários.R

---
+++ Pacote necessários.R
@@ -0,0 +1,10 @@
+#Pacotes necessarios para o projeto
+
+install.packages(readxl)
+install.packages (dplyr)
+install.packages (wordcloud)
+install.packages (tm)
+install.packages (RColorBrewer)
+install.packages (cluster)
+install.packages (ggplot2)
+install.packages (fpc)
